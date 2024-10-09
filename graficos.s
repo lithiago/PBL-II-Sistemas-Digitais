db729127984fe84ed080ecf27b0a890f61cee590
GuilhermeLopes <146552217+GuilhermeFRLopes@users.noreply.github.com>
2024-10-09 10:15:52 -0300

    Update graficos.s

--- graficos.s
+++ graficos.s
@@ -1,8 +1,12 @@
 .global main1
 .global subtrai
+.global multiplica
+
 
 .type main1, %function
 .type subtrai, %function
+.type multiplica, %function
+
 
 main1:
     adds r0, r0, r1
@@ -10,4 +14,8 @@ main1:
 
 subtrai:
     subs r0, r0, r1
+    bx lr
+
+multiplica:
+    lsl r0, #3
     bx lr
