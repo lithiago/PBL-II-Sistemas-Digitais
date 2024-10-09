9045a0ffc1ad89641a5f9e547409f84baaddab0a
GuilhermeLopes <146552217+GuilhermeFRLopes@users.noreply.github.com>
2024-10-09 10:16:16 -0300

    Update test.c

--- test.c
+++ test.c
@@ -3,6 +3,13 @@
 int main() {
     int soma = main1(2, 3);
     printf("\nresultado soma de 2 + 3: %d\n", soma);
+    
     int sub = subtrai(soma, 1);
     printf("\nresultado da subtracao: %d\n", sub);
+
+    printf("valor atual da soma: %d\n",soma);
+
+
+    int mult = multiplica(soma);
+    printf("multiplica: %d\n", mult);
 }
