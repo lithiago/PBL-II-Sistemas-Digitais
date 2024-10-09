65f2e18100577e435df95f57830b850418c057a0
GuilhermeLopes <146552217+GuilhermeFRLopes@users.noreply.github.com>
2024-10-09 09:56:34 -0300

    Update soma.s

--- soma.s
+++ soma.s
@@ -1,16 +1,6 @@
-.section .data                ; Seção de dados
-    msg: .asciz "Hello, World!\n" ; Mensagem a ser exibida
+.global main1
+.type main1, %function
 
-.section .text                ; Seção de código
-    .global hello_world       ; Tornar a função global
-
-hello_world:                  ; Início da função
-    ldr r0, =1                ; Descritor de arquivo 1 (stdout)
-    ldr r1, =msg              ; Endereço da mensagem
-    ldr r2, =14               ; Comprimento da mensagem (14 bytes)
-
-    ; Chamada de sistema para escrever na saída padrão
-    mov r7, #4                ; Número da chamada de sistema para sys_write
-    svc 0                     ; Executa a chamada de sistema
-
-    bx lr                     ; Retorna da função
+main1:
+    adds r0, r0, r1
+    bx lr
