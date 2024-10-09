adf30e621c4bc804b6ebc8c5597c4b7160001d0f
Thiago Ramon <133051391+lithiago@users.noreply.github.com>
2024-10-09 09:12:29 -0300

    Update teste2.c

--- teste2.c
+++ teste2.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
 
 // Declaração da função Assembly
-extern int somaValores(int valor1, int valor2);
+extern void hello_world();
 
 int main() {
-    int valor1 = 10;
-    int valor2 = 20;
-
-    // Chama a função Assembly para somar os dois valores
-    int resultado = somaValores(valor1, valor2);
-
-    // Exibe o resultado da soma
-    printf("Resultado da soma: %d\n", resultado);
+    // Chama a função Assembly para imprimir "Hello, World!"
+    hello_world();
 
     return 0;
 }
