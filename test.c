6bb2b45787e3a475062f08b584791076d6b3cb2c
GuilhermeLopes <146552217+GuilhermeFRLopes@users.noreply.github.com>
2024-10-09 10:09:46 -0300

    Update test.c

--- test.c
+++ test.c
@@ -2,5 +2,7 @@
 
 int main() {
     int soma = main1(2, 3);
-    printf("\nresultado soma: %d\n", soma);
+    printf("\nresultado soma de 2 + 3: %d\n", soma);
+    int sub = subtrai(soma, 1);
+    printf("\nresultado da subtracao: %d\n", sub);
 }
