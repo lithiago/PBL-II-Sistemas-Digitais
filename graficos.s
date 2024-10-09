7f11ab50a4ece455f3d786182d71c21a3ea915d5
Thiago Ramon <133051391+lithiago@users.noreply.github.com>
2024-10-09 08:38:34 -0300

    Update graficos.s

--- graficos.s
+++ graficos.s
@@ -2,8 +2,9 @@
 draw_pixel:
     push {r4, r5, r6, r7, lr}     @ Salva registradores R4, R5, R6, R7 e LR
 
-    @ Endereço base da memória de vídeo
-    ldr r6, =0xFF200000           @ r6 contém o endereço base da memória de vídeo (VGA)
+    @ Endereço base da memória de vídeo (VGA), usando movw e movt
+    movw r6, #:lower16:0xFF200000  @ Carrega a parte baixa do endereço
+    movt r6, #:upper16:0xFF200000  @ Carrega a parte alta do endereço
 
     @ Parâmetros recebidos:
     @ r0: coordenada y
@@ -21,7 +22,7 @@ draw_pixel:
     add r7, r7, r4                @ r7 = (y * 640) + x
 
     @ Escrever a cor no endereço calculado (multiplicado por 4)
-    str r5, [r6, r7, lsl #2]      
+    str r5, [r6, r7, lsl #2]      @ Escreve a cor no endereço calculado
 
     pop {r4, r5, r6, r7, lr}      @ Restaura os registradores
     bx lr                         @ Retorna da função
