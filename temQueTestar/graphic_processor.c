667e91d4d1e916ccc5c498b2ad8be1e9ea95b1f8
GuilhermeLopes <146552217+GuilhermeFRLopes@users.noreply.github.com>
2024-10-24 00:35:18 -0300

    Update graphic_processor.c

--- temQueTestar/graphic_processor.c
+++ temQueTestar/graphic_processor.c
@@ -297,11 +297,11 @@ int collision(Sprite *sp1, Sprite *sp2){
 
 void setQuadrado(int coluna, int linha, int R, int G, int B){
     // Ajuste para desenhar um quadrado 4x4
-    for (int i = 0; i < 4; i++){
-        for (int j = 0; j < 4; j++){
+    for (int i = 0; i < 10; i++){
+        for (int j = 0; j < 10; j++){
             // Aumenta as coordenadas para o bloco
-            int block_col = (coluna * 4) + j;
-            int block_line = (linha * 4) + i;
+            int block_col = (coluna * 10) + j;
+            int block_line = (linha * 10) + i;
 			usleep(1000);
             // Chama a função para definir o bloco de fundo
             set_background_block(block_col, block_line, R, G, B);
